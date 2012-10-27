e1518894a732d47f9be7a026d6c25f6fc6fa4d59
Hartmut Goebel <h.goebel@crazy-compilers.com>
2012-10-27 18:12:09 +0200

    Putting myself as the first author, since I took over.

--- gitflow/__init__.py
+++ gitflow/__init__.py
@@ -12,8 +12,8 @@ repository operations for Vincent Driessen's branching model.
 VERSION = (0, 5, 0)
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
-__author__ = "Vincent Driessen, Hartmut Goebel"
-__contact__ = "vincent@datafox.nl, h.goebel@goebel-consult.de"
+__author__ = "Hartmut Goebelm, Vincent Driessen"
+__contact__ = "h.goebel@crazy-compilers.com, vincent@datafox.nl"
 __homepage__ = "http://github.com/nvie/gitflow/"
 __docformat__ = "restructuredtext"
 __copyright__ = "2010-2011 Vincent Driessen; 2012 Hartmut Goebel"
