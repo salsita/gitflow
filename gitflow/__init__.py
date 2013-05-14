9ca040a5b775688ff8bde57c1c245946f81d9e37
Ondrej Kupka <ondra.cap@gmail.com>
2013-05-14 10:35:06 +0200

    Bump version to 0.7.0rc9

--- gitflow/__init__.py
+++ gitflow/__init__.py
@@ -9,7 +9,7 @@ repository operations for Vincent Driessen's branching model.
 # Distributed under a BSD-like license. For full terms see the file LICENSE.txt
 #
 
-VERSION = (0, 7, 0, 'rc7')
+VERSION = (0, 7, 0, 'rc9')
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ondrej Kupka, Tomas Brambora, Hartmut Goebelm, Vincent Driessen"
