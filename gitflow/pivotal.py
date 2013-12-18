737819aabf7258130f9d00a6190eb83df2a69a30
Ondrej Kupka <ondra.cap@gmail.com>
2013-12-18 13:39:19 +0100

    pivotal: Fix Story.is_unstarted to include unscheduled

--- gitflow/pivotal.py
+++ gitflow/pivotal.py
@@ -134,7 +134,7 @@ class Story(object):
         return self.get_state() == 'started'
 
     def is_unstarted(self):
-        return self.get_state() == 'unstarted'
+        return self.get_state() == 'unstarted' or self.get_state() == 'unscheduled'
 
     def set_me_as_owner(self):
         headers = {'X-TrackerToken': _get_token()}
